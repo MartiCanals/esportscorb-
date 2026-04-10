2dbbeab02e54d871d564894ec74c90205e9f8b86
MartiCanals <canals.marti@gmail.com>
2026-04-10 11:05:42 +0200

    Fix database routing for Vercel

--- esportscorb/settings.py
+++ esportscorb/settings.py
@@ -82,11 +82,16 @@ import dj_database_url
 # ... entremig hi haurà el SECRET_KEY, etc. ...
 
 # Busca la part de DATABASES i deixa-la així:
+import dj_database_url
+import os
+
+# ... (altre codi)
+
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
-        ssl_require=True
+        conn_health_checks=True,
     )
 }
 
